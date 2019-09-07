bc923423105bb7e4497c2ce92db6803175db34e1
RedGuys <admin@redguy.ru>
2019-09-07 21:24:19 +0300

    main class hotfix

--- src/main.java
+++ src/main.java
@@ -2,13 +2,14 @@ import RedNetWorker.Clients.Enums.HttpLibrary;
 import RedNetWorker.Clients.HttpClient;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class main {
     public static void main(String[] args) {
         HttpClient client = new HttpClient(HttpLibrary.JavaNet);
         try {
             System.out.print(client.getString("https://google.com"));
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
     }
