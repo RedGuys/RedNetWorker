ca61fe09a8166299a223a807076a1f44bce8f10b
RedGuys <admin@redguy.ru>
2019-09-07 18:52:38 +0300

    Http Java.net Client - post method

--- src/RedNetWorker/Clients/HttpClient.java
+++ src/RedNetWorker/Clients/HttpClient.java
@@ -3,11 +3,9 @@ package RedNetWorker.Clients;
 import RedNetWorker.Clients.Enums.HttpLibrary;
 import RedNetWorker.Utils.Url;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.Proxy;
 import java.net.URL;
 import java.util.Map;
@@ -79,7 +77,57 @@ public class HttpClient {
             return "";
         }
     }
+
     public String getString(String url) throws IOException {
+        return getString(url,null);
+    }
+
+    //post's
+    public InputStream post(String uri, Map<String,String> args) throws IOException {
+        URL url = new URL(uri);
+        switch (library) {
+            case JavaNet:
+                HttpURLConnection connection;
+                if(proxy != null) {
+                    connection = (HttpURLConnection) url.openConnection(this.proxy);
+                } else {
+                    connection = (HttpURLConnection) url.openConnection();
+                }
+                connection.setRequestMethod("POST");
+                connection.setConnectTimeout(this.connectionTimeout);
+                connection.setReadTimeout(this.readTimeout);
+                if(args != null) {
+                    connection.setDoOutput(true);
+                    DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+                    out.writeBytes(Url.getParamsString(args));
+                    out.flush();
+                    out.close();
+                }
+                return connection.getInputStream();
+        }
+        return null;
+    }
+
+    public InputStream post(String uri) throws IOException {
+        return post(uri,null);
+    }
+
+    public String postString(String url, Map<String,String> args) throws IOException {
+        InputStream stream = post(url, args);
+        try (final BufferedReader in = new BufferedReader(new InputStreamReader(stream))) {
+            String inputLine;
+            final StringBuilder content = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            return content.toString();
+        } catch (final Exception ex) {
+            ex.printStackTrace();
+            return "";
+        }
+    }
+
+    public String postString(String url) throws IOException {
         return getString(url,null);
     }
 }
