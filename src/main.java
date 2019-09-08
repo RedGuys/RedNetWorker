e44e2ec52a1ddd40c0278eaf97e9c1654f2bdf6f
RedGuys <admin@redguy.ru>
2019-09-08 12:00:12 +0300

    MainUpdate

--- src/main.java
+++ src/main.java
@@ -3,6 +3,8 @@ import RedNetWorker.Clients.Enums.HttpLibrary;
 import RedNetWorker.Clients.FTPClient;
 import RedNetWorker.Clients.HttpClient;
 import RedNetWorker.Utils.Logger;
+import it.sauronsoftware.ftp4j.FTPException;
+import it.sauronsoftware.ftp4j.FTPIllegalReplyException;
 import sun.rmi.runtime.Log;
 
 import java.io.File;
@@ -50,14 +52,44 @@ public class main {
             e.printStackTrace();
         }
         Logger.info("   FTP");
+        Logger.info("apacheFTPClient");
         FTPClient ftpClient = new FTPClient(FTPLibrary.apacheFTPClient);
-        ftpClient.Connect("speedtest.tele2.net");
+        try {
+            ftpClient.Connect("speedtest.tele2.net");
+        } catch (FTPException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (FTPIllegalReplyException e) {
+            e.printStackTrace();
+        }
         try {
             ftpClient.LoginAnonymous();
             Logger.info("Downloading");
             if(ftpClient.downloadFile("tests/apacheFTPClient.zip","10MB.zip")) {
                 Logger.info("Uploading");
                 ftpClient.uploadFile("tests/apacheFTPClient.zip","/upload/apacheFTPClient.zip");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Logger.info("ftp4jLibrary");
+        ftpClient = new FTPClient(FTPLibrary.ftp4jFTPClient);
+        try {
+            ftpClient.Connect("speedtest.tele2.net");
+        } catch (FTPException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (FTPIllegalReplyException e) {
+            e.printStackTrace();
+        }
+        try {
+            ftpClient.LoginAnonymous();
+            Logger.info("Downloading");
+            if(ftpClient.downloadFile("tests/ftp4jFTPClient.zip","10MB.zip")) {
+                Logger.info("Uploading");
+                ftpClient.uploadFile("tests/ftp4jFTPClient.zip","/upload/ftp4jFTPClient.zip");
             }
         } catch (IOException e) {
             e.printStackTrace();
