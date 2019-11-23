f71b3bba3b13c8f91924981729b1c45fbe190d4b
RedGuy <admin@redguy.ru>
2019-11-24 01:53:45 +0300

    SFTPClient - Fixes, ls and cd voids

--- src/RedNetWorker/Clients/SFTPClient/SFTPClient.java
+++ src/RedNetWorker/Clients/SFTPClient/SFTPClient.java
@@ -2,17 +2,22 @@ package RedNetWorker.Clients.SFTPClient;
 
 import RedNetWorker.Clients.Enums.SFTPLibrary;
 import RedNetWorker.Clients.SFTPClient.SFTPExceptions.OpenConnectionException;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
+import RedNetWorker.Clients.SFTPClient.SFTPExceptions.ServerMethodErrorException;
+import RedNetWorker.Utils.Logger;
+import com.jcraft.jsch.*;
+
+import java.util.Vector;
 
 public class SFTPClient {
     private SFTPLibrary library;
     private String host;
     private String user;
     private int port;
+    private String workingDirectory = "/";
+
     private JSch jsch;
     private Session jschSession;
+    private ChannelSftp channelSftp;
 
     public SFTPClient(SFTPLibrary library) {
         this.library = library;
@@ -44,12 +49,51 @@ public class SFTPClient {
                 try {
                     this.jsch.setKnownHosts(knownHostsFile);
                     this.jschSession = jsch.getSession(user, host);
-                    jschSession.setPassword(password);
-                    jschSession.connect();
+                    this.jschSession.setPassword(password);
+                    this.jschSession.connect(); //TODO: Fix java.net.ConnectException: Connection refused: connect
+                    this.channelSftp = (ChannelSftp)jschSession.openChannel("sftp");
+                    this.channelSftp.connect();
                 } catch (JSchException e) {
                     throw new OpenConnectionException(e.getMessage(),this.host,this.port,e.getCause());
                 }
                 break;
         }
+    }
+
+    public void setWorkingDirectory(String workingDirectory) throws ServerMethodErrorException {
+        this.workingDirectory = workingDirectory;
+        switch (library) {
+            case jsch:
+                try {
+                    channelSftp.cd(this.workingDirectory);
+                } catch (SftpException e) {
+                    throw new ServerMethodErrorException(e.getMessage(),this.host,this.port,e.getCause());
+                }
+                break;
+        }
+    }
+
+    public String getWorkingDirectory() {
+        return this.workingDirectory;
+    }
+
+    public SFTPFile ls() throws ServerMethodErrorException {
+        return ls(this.workingDirectory);
+    }
+
+    public SFTPFile ls(String path) throws ServerMethodErrorException {
+        switch (library) {
+            case jsch:
+                try {
+                    Vector files = channelSftp.ls(path);
+                    for (Object file : files) {
+                        Logger.info(file);
+                    }
+                    return null;
+                } catch (SftpException e) {
+                    throw new ServerMethodErrorException(e.getMessage(),this.host,this.port,e.getCause());
+                }
+        }
+        return null;
     }
 }
