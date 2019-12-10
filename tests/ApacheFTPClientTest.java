83bb2354f9ea51e46b23cdf200ec2f4f68960cab
RedGuy <admin@redguy.ru>
2019-12-10 21:36:34 +0300

    New Test, fixes

--- tests/ApacheFTPClientTest.java
+++ tests/ApacheFTPClientTest.java
@@ -1,38 +1,36 @@
 import ru.redguy.rednetworker.Utils.Logger;
+import ru.redguy.rednetworker.clients.ftp.ApacheFTPClient;
 import ru.redguy.rednetworker.clients.ftp.FTPFile;
-import ru.redguy.rednetworker.clients.ftp.Ftp4jFTPClient;
 
 import java.io.File;
 
-
-public class Ftp4jFTPClientTest {
+public class ApacheFTPClientTest {
     public static void main(String[] args) throws Exception {
-        Ftp4jFTPClient ftp4jFTPClient = new Ftp4jFTPClient();
-        ftp4jFTPClient.connect("speedtest.tele2.net");
-        ftp4jFTPClient.loginAnonymous();
-        FTPFile[] files = ftp4jFTPClient.list();
+        ApacheFTPClient apacheFTPClient = new ApacheFTPClient();
+        apacheFTPClient.connect("speedtest.tele2.net");
+        apacheFTPClient.loginAnonymous();
+        FTPFile[] files = apacheFTPClient.list();
         if(files.length == 18) {
             Logger.info("list - ok!");
         } else {
             Logger.error("list - error");
             throw new Exception("Illegal result");
         }
-        ftp4jFTPClient.downloadFile("Ftp4jFTPClientTest.test","/100MB.zip");
-        if(new File("Ftp4jFTPClientTest.test").length() == 104857600L)  {
+        apacheFTPClient.downloadFile("ApacheFTPClientTest.test","/100MB.zip");
+        if(new File("ApacheFTPClientTest.test").length() == 104857600L)  {
             Logger.info("download - ok!");
         } else {
             Logger.error("download - error");
             throw new Exception("Illegal result");
         }
-        ftp4jFTPClient.cd("upload");
-        if(ftp4jFTPClient.getWorkingDirectory().equals("/upload")) {
+        apacheFTPClient.cd("upload");
+        if(apacheFTPClient.getWorkingDirectory().equals("/upload")) {
             Logger.info("cd - ok!");
         } else {
             Logger.error("cd - error");
             throw new Exception("Illegal result");
         }
-        ftp4jFTPClient.remoteRenameBlockFix = true;
-        ftp4jFTPClient.uploadFile("Ftp4jFTPClientTest.test","downloaded.zip");
+        apacheFTPClient.uploadFile("ApacheFTPClientTest.test","downloaded.zip");
         Logger.info("upload - ok!");
     }
 }
