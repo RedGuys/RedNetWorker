9c0b32889adc4eb72290a60714919414d70fa204
Ilya <admin@redguy.ru>
2020-11-21 19:05:21 +0300

    new response system

---
+++ clients/http/src/main/java/ru/redguy/rednetworker/clients/http/IHttpResponse.java
@@ -0,0 +1,18 @@
+package ru.redguy.rednetworker.clients.http;
+
+import ru.redguy.rednetworker.clients.http.exceptions.HttpProtocolException;
+import ru.redguy.rednetworker.clients.http.exceptions.InputStreamException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public interface IHttpResponse {
+    public String getString() throws HttpProtocolException, InputStreamException;
+
+    public InputStream getInputStream() throws HttpProtocolException, InputStreamException;
+
+    public File saveToFile(String pathToFile) throws IOException;
+
+    public File saveToFile(File file) throws IOException;
+}
