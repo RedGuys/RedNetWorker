773e0514318960768fb470642cd7c8e53a06108d
Ilya <admin@redguy.ru>
2020-11-21 19:16:07 +0300

    Update http tests

--- clients/http/src/test/java/Apache.java
+++ clients/http/src/test/java/Apache.java
@@ -4,7 +4,7 @@ import ru.redguy.rednetworker.clients.http.IHttpClient;
 public class Apache {
     public static void main(String[] args) throws Exception {
         IHttpClient client = new ApacheHttpClient();
-        System.out.println(client.getString("https://tests.redguy.ru/robots.txt"));
-        System.out.println(client.postString("https://tests.redguy.ru/robots.txt"));
+        System.out.println(client.get("https://tests.redguy.ru/robots.txt").getString());
+        System.out.println(client.post("https://tests.redguy.ru/robots.txt").getString());
     }
 }
--- clients/http/src/test/java/Fluent.java
+++ clients/http/src/test/java/Fluent.java
@@ -4,7 +4,7 @@ import ru.redguy.rednetworker.clients.http.IHttpClient;
 public class Fluent {
     public static void main(String[] args) throws Exception {
         IHttpClient client = new ApacheFluentAPI();
-        System.out.println(client.getString("tests.redguy.ru/robots.txt"));
-        System.out.println(client.postString("https://tests.redguy.ru/robots.txt"));
+        System.out.println(client.get("tests.redguy.ru/robots.txt").getString());
+        System.out.println(client.post("https://tests.redguy.ru/robots.txt").getString());
     }
 }
