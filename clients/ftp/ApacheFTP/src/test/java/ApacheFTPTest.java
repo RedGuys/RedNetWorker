f0f0e8ad01990ea6aa9cf22482bc40f823bf6cfa
Ilya <admin@redguy.ru>
2021-07-15 20:09:43 +0300

    Add TODO to do test

--- clients/ftp/ApacheFTP/src/test/java/ApacheFTPTest.java
+++ clients/ftp/ApacheFTP/src/test/java/ApacheFTPTest.java
@@ -1,7 +1,6 @@
-import ru.redguy.rednetworker.clients.ftp.ApacheFTPClient;
-
 public class ApacheFTPTest {
-     public static void main(String[] args) {
-         ApacheFTPClient apacheFTPClient = new ApacheFTPClient();
-     }
+    public static void main(String[] args) {
+        //TODO: implement test
+        System.out.println("Test not implemented!");
+    }
 }
